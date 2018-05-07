e2e0f71fcb18d84dd886d1d74c276a495b1a48cd
Hyun Do Park <hyundopark@gmail.com>
2018-05-06 21:41:36 -0700

    other team member contribution deleted

--- ReactForm/DogOwnerRoutes.js
+++ ReactForm/DogOwnerRoutes.js
@@ -1,13 +1,23 @@
-const router = require('express').Router()
-const dogOwnersController = require('../controllers/dogOwnersController')
-// other team member contribution deleted
-const DogOwner = require('../models/dogOwner')
-// other team member contribution deleted
+const router = require("express").Router();
+const dogOwnersController = require("../controllers/dogOwnersController");
+//other team member contribution
+const DogOwner = require("../models/dogOwner");
+//other team member contribution
 
-module.exports = router
+module.exports = router;
 
-router.post('/', // other team member contribution deleted // , dogOwnersController.create)
-router.get('/:id([0-9a-fA-F]{24})', dogOwnersController.readById)
-router.delete('/:id([0-9a-fA-F]{24})', dogOwnersController.delete)
-router.get('/', dogOwnersController.readAll)
-router.put('/:id([0-9a-fA-F]{24})' // other team member contribution deleted // , dogOwnersController.update)
+router.post(
+  "/",
+ //other team member contribution
+ //other team member contribution
+  dogOwnersController.create
+);
+router.get("/:id([0-9a-fA-F]{24})", dogOwnersController.readById);
+router.delete("/:id([0-9a-fA-F]{24})", dogOwnersController.delete);
+router.get("/", dogOwnersController.readAll);
+router.put(
+  "/:id([0-9a-fA-F]{24})",
+  //other team member contribution
+  //other team member contribution
+  dogOwnersController.update
+);
